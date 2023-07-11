bfcfce244dc8838156379acd222397f43292c2df
Zwe Sithu <25-00214@dat.com>
2023-07-11 16:11:56 +0630

    feat:add burmese numbers localization

--- aufzeichnungen/helpers.py
+++ aufzeichnungen/helpers.py
@@ -6,9 +6,29 @@
 """ validation """
 """ validation ends """
 
-"""datasets"""
+"""helper classes"""
 from typing import Any
 
+MM_NUMBERS = ["၀", "၁", "၂", "၃", "၄", "၅", "၆", "၇", "၈", "၉"]
+
+class BurInt():
+    def __init__(self, value) -> None:
+        match value:
+            case str() if value.isnumeric():
+                pass
+            case int():
+                value = str(value)
+            case _:
+                raise ValueError("Please provide an int or numeric input")
+        self.value = value
+        self._getBurmeseNumber()
+
+    def _getBurmeseNumber(self):
+        self.burmese_value =  "".join([MM_NUMBERS[int(i)] for i in self.value])
+    
+    def formatBurmeseValue(self):
+        b, a = divmod(len(self.burmese_value), 3)
+        return ",".join(([self.burmese_value[:a]] if a else []) + [self.burmese_value[a + 3 * i : a + 3 * i + 3] for i in range(b)])
 
 class ReadonlyDict(dict):
     __read_only__ = True
